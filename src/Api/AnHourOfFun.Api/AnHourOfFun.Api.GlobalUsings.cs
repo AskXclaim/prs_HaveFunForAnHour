4186ccb61dd35674859cb3ce8f82272f7ed7f846
Kola <ask_kol@yaoo.co.uk>
2023-09-08 14:44:55 +0100

    Add Api Controller - may still need some modification

---
+++ src/Api/AnHourOfFun.Api/AnHourOfFun.Api.GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using GameStation.Interfaces;
+global using GameStation.RockPaperScissor;
+global using Microsoft.AspNetCore.Mvc;
---
+++ src/Api/AnHourOfFun.Api/Controllers/TwoPlayerRpsGameController.cs
@@ -0,0 +1,36 @@
+namespace AnHourOfFun.Api.Controllers;
+
+public class TwoPlayerRpsGameController : Controller
+{
+    private readonly ILogger<TwoPlayerRpsGameController> _logger;
+    private readonly IGame _game;
+    private readonly IGameRules _gameRules;
+
+    public TwoPlayerRpsGameController(ILogger<TwoPlayerRpsGameController> logger,
+        IGame game, IGameRules gameRules)
+    {
+        _logger = logger;
+        _game = game;
+        _gameRules = gameRules;
+    }
+
+    [HttpPost]
+    [Route(nameof(Play))]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<IPlayer>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<IEnumerable<RockPaperScissorPlayer>> Play(List<RockPaperScissorPlayer> players)
+    {
+        try
+        {
+            var iPlayers = players.Cast<IPlayer>().ToList();
+            var result = _game.Play(iPlayers, _gameRules);
+            return Ok(result);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, $"An error occurred while calling {nameof(Play)} with parameters;" +
+                                        $"{Environment.NewLine}{nameof(players)}:{players}");
+            throw;
+        }
+    }
+}
